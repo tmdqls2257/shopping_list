90bca79b0d93b9b3026c53d558e117cf0c027b56
tmdqls2257 <54207332+tmdqls2257@users.noreply.github.com>
2021-06-12 17:33:10 +0900

    success shopping__list

--- main.js
+++ main.js
@@ -1,48 +1,57 @@
-
-const Ul = document.createElement('ul'),
-  plusClick = document.querySelector('.plus'),
-  Input = document.querySelector('.footer__input');
-
-function addRemoveList() {
-
-
-    Input.value = '';
-    Image.addEventListener("click", () =>{
-      Ul.removeChild(li);
-    });
-    if(Input.value === ''){
-      input.focus();
-      return;
-    }
-}
+const Ul = document.querySelector('.items');
+const input =  document.querySelector('.footer__input');
+const addBtn = document.querySelector('.footer__button');
 
 function onAdd(){
-  const text = input.value;
-  console.log(text);
-  const item = createItem();
+  if (input.value === ''){
+    input.focus();
+    return;
+  }
+  console.log(input.value);
+  const item = makeItem();
   Ul.appendChild(item);
+// 새로 추가된 아이템으로 스크롤링
+  item.scrollIntoView({blck: 'center'});
+// 5. 인풋을 초기화
   input.value = '';
   input.focus();
 }
 
-function createItem(){
-  const 
-  li = document.createElement("li"),
-  span = document.createElement("span"),
-  Image = document.createElement("i");
-    li.setAttribute("class", "ul__list");
-    span.setAttribute("class", "list__item");
-    Image.setAttribute("class", "fas fa-trash-alt trash");
-    Section.appendChild(Ul);
-    Ul.appendChild(li);
-    li.appendChild(span);
-    li.appendChild(Image);
+function makeItem(){
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item__row');
+
+  const item = document.createElement('div');
+  item.setAttribute('class', 'item');
+  itemRow.appendChild(item);
+
+  const itemDivider = document.createElement('div');
+  itemDivider.setAttribute('class', 'item__divider');
+  itemRow.appendChild(itemDivider);
+  
+  const name = document.createElement('span');
+  name.setAttribute('class', 'item__name');
+  name.innerText = input.value;
+  item.appendChild(name);
+
+  const img = document.createElement('i');
+  const Btn = document.createElement('button');
+  Btn.setAttribute('class', 'item__delete');
+  img.setAttribute('class', 'fas fa-trash-alt');
+  item.appendChild(Btn);
+  Btn.appendChild(img);
+  Btn.addEventListener("click", () => {
+    itemRow.remove();
+  })
+  return itemRow;
 }
 
-plusClick.addEventListener("click", () => {
-  addRemoveList();
-  });
+addBtn.addEventListener('click', () => {
+  onAdd();
+});
 
-Input.addEventListener("keypress", () => {
-    addRemoveList();
-    });
+input.addEventListener('keypress', event => {
+  if(event.keyCode === 13){
+    onAdd();
+  }
+})
